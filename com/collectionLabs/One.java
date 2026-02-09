dcb00202d9855c1840de1d5721167d99558ab029
Javac-g <denysov.maxym@gmail.com>
2026-02-09 15:46:23 +0200

    primitives arrays created / use cases

---
+++ com/collectionLabs/One.java
@@ -0,0 +1,27 @@
+package com.collectionLabs;
+
+import java.util.Arrays;
+
+public class One {
+
+
+
+    public static void one(){
+        int[] arr1 = new int[3];
+        int[] arr2, arr3;
+        int arr4[], arr5;
+        int[] arr6 = {1,2,3,4,5,6};
+        int[] arr7 = new int[]{1,2};
+
+    }
+    public static void main(String... args){
+
+
+
+
+
+
+
+
+    }
+}
